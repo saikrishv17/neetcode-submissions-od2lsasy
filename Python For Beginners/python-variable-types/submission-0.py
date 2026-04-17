cdd089c36ef75e73464325790da4d6697b336d0d
Saikrish "Sai" Venkataraman <saikrishv17@gmail.com>
2026-04-17 10:11:21 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 10
+float_type = 10.0
+bool_type = True
+string_type = "Bro"
+list_type = [integer_type, float_type, bool_type, string_type]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
