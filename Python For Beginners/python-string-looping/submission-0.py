e97a44d85b459d6ef32529e38ad50d34b3d5c5fb
Saikrish "Sai" Venkataraman <saikrishv17@gmail.com>
2026-04-25 22:17:34 -0400

    Add: python-string-looping - submission-0

---
+++ Python For Beginners/python-string-looping/submission-0.py
@@ -0,0 +1,8 @@
+def print_string_characters(my_string: str) -> None:
+    for x in range(len(my_string)):
+        print(my_string[x])
+
+
+# do not modify below this line
+print_string_characters("Hello, World!")
+print_string_characters("Good Job!")
